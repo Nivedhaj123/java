f1b3098ac109e199dd800c521a093dfeaff95940
Nivedhaj123 <94376216+Nivedhaj123@users.noreply.github.com>
2021-12-15 13:56:01 +0530

    nive

---
+++ basic.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+public class java
+{
+    public static void main(String args[ ])
+    {
+        Scanner dc=new Scanner(System.in);
+        int num=dc.nextInt();
+        System.out.println(num);
+        
+    }
+}
